c364008f72d6c7ab38478ef888ff52711bed29d7
Daniel Dreier <d@puppetlabs.com>
2015-03-26 18:10:17 -0700

    switch from contain to include for apt

--- manifests/repository/debian.pp
+++ manifests/repository/debian.pp
@@ -4,7 +4,7 @@
 #
 class riak2::repository::debian {
   ensure_packages('apt-transport-https') # packagecloud is https-only
-  contain ::apt
+  include ::apt
   apt::source { 'riak':
     location   => 'https://packagecloud.io/basho/riak/debian/',
     key        => '418A7F2FB0E1E6E7EABF6FE8C2E73424D59097AB',
